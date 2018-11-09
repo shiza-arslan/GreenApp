4d8866d4757f1c98bd01d22af1998c307d97468e
Shiza <shizakhaliq2200@gmail.com>
2018-11-08 23:16:02 -0800

    commit

---
+++ GreenApp-SPA/src/app/bussiness.ts
@@ -0,0 +1,10 @@
+export class Bussiness {
+    constructor(
+        public id: number,
+        public Name: string,
+        public City: string,
+        public Address: string,
+        public Email: String,
+        public Password: string
+      ) { }
+}
